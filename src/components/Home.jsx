f9fbea89f365703a3639f611e11ce1d0c66531e6
rajvi0106 <rajvisinghrathore001@gmail.com>
2026-06-22 00:57:45 +0530

    improve home page text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,10 @@ function Home() {
     return (
         <div className="bg-purple-400 flex justify-between">
             <div className="text-white flex flex-col w-1/2 h-screen">
-                <p className="text-7xl ml-10 mt-50 mb-5">Vibe Nearby</p>
-                <p className="ml-10">lorem ipsum dolor sit amet consectetur adipisicing elit. Sed inventore dolorem natus, odit incidunt nobis culpa dolores hic deserunt quam quos aliquid. Esse ipsum praesentium accusamus iste, vero corrupti voluptatem.lorem ipsum dolor sit amet consectetur adipisicing elit. Sed inventore dolorem natus, odit incidunt nobis culpa dolores hic deserunt quam quos aliquid. Esse ipsum praesentium accusamus iste, vero corrupti voluptatem.</p>
+                <p className="text-7xl ml-10 mt-50 mb-5">Find your vibe, Nearby</p>
+                <br></br>
+                <br></br>
+                <p className="ml-10 text-2xl">Tell us what you're in the mood for , a quick bite, some shopping, a quiet spot to read, or a night out and we'll find real places near you that match. Set your budget, share your location, and get a curated list with ratings, opening hours, and distance, mapped out so you know exactly where to go.</p>
             </div>
             <div>
                 <img src="https://images.pexels.com/photos/16856995/pexels-photo-16856995.jpeg" className="h-200 mt-10"></img>
